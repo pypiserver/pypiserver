74ff96387c50c91eb6a8a75c1052dffdeda65cde
Ralf Schmitt <ralf@systemexit.de>
2011-08-01 00:34:38 +0200

    show some minimal information for /

--- pypiserver/core.py
+++ pypiserver/core.py
@@ -44,8 +44,30 @@ class pkgset(object):
 
 @route('/')
 def root():
-    return request.url
-    redirect("/simple/")
+    try:
+        numpkgs = len(packages.find_packages())
+    except:
+        numpkgs = 0
+
+    return """<html><head><title>Welcome to pypiserver!</title></head><body>
+<h1>Welcome to pypiserver!</h1>
+<p>This is a PyPI compatible package index serving %(NUMPKGS)s packages.</p>
+
+<p> To use this server with pip, run the the following command:
+<blockquote><pre>
+pip install -i %(URL)ssimple PACKAGE [PACKAGE2...]
+</pre></blockquote></p>
+
+<p> To use this server with easy_install, run the the following command:
+<blockquote><pre>
+easy_install -i %(URL)ssimple PACKAGE
+</pre></blockquote></p>
+
+<p>The complete list of all packages can be found <a href="/packages/">here</a> or via the <a href="/simple/">/simple</a> index.</p>
+
+<p>This instance is running version %(VERSION)s of the <a href="http://pypi.python.org/pypi/pypiserver">pypiserver</a> software.</p>
+</body></html>
+""" % dict(URL=request.url, VERSION=__version__, NUMPKGS=numpkgs)
 
 
 @route("/simple")
@@ -73,6 +95,7 @@ def simple(prefix=""):
     return "".join(res)
 
 
+@route('/packages')
 @route('/packages/')
 def list_packages():
     files = packages.find_packages()
