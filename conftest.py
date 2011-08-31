7a4a6a2d97507e9489b96c7a1147ddcb35651629
Ralf Schmitt <ralf@systemexit.de>
2011-09-01 00:50:56 +0200

    work around issue caused by mechanize when using pypy to run tests

---
+++ conftest.py
@@ -0,0 +1,7 @@
+import sys
+import httplib
+
+def pytest_configure(config):
+    if hasattr(sys, "pypy_version_info"):
+        # mechanize (as included by twill) calls this
+        httplib.HTTPResponse._decref_socketios = lambda self: None
