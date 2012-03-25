7f97612a7ed6cf98b495dd5ad1faef86a4800c1c
Ralf Schmitt <ralf@systemexit.de>
2012-03-26 00:27:43 +0200

    fix pytz issue #6
    relax the version regex a bit.

--- pypiserver/core.py
+++ pypiserver/core.py
@@ -21,13 +21,13 @@ config = configuration()
 
 
 def guess_pkgname(path):
-    pkgname = re.split(r"-\d+\.", os.path.basename(path))[0]
+    pkgname = re.split(r"-\d+", os.path.basename(path))[0]
     return pkgname
 
 
 def guess_pkgname_and_version(path):
     path = os.path.basename(path)
-    pkgname = re.split(r"-\d+\.", path, 1)[0]
+    pkgname = re.split(r"-\d+", path, 1)[0]
     version = path[len(pkgname) + 1:]
     version = re.sub(r"(\.zip|\.tar\.gz|\.tgz|\.tar\.bz2)$", "", version)
     return pkgname, version
