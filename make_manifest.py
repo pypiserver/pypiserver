be52ec1c40ccbe13c1e65812a39675893321a7a0
Ralf Schmitt <ralf@systemexit.de>
2012-12-02 01:37:01 +0100

    fix .travis.yml, tox.ini
    python 2.5 needs an older webob version. so, we add the dependency in
    tox.ini. we use tox to run the tests on travis.
    
    remove python 3.3 from travis.yml as it's not there yet.

--- make_manifest.py
+++ make_manifest.py
@@ -6,7 +6,7 @@ import os
 def main():
     files = sorted(set([x.strip() for x in os.popen("git ls-files")]) -
                    set(("make_manifest.py", "commit-standalone",
-                        "vendor", ".gitmodules", ".travis.yml")))
+                        "vendor", ".gitmodules", ".travis.yml", ".travis-runtox.py")))
     with open("MANIFEST.in", "w") as f:
         for x in files:
             f.write("include %s\n" % x)
