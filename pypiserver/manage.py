44191af49bddf26ba18a3786cee4cac641042a26
Ralf Schmitt <ralf@systemexit.de>
2012-12-26 23:26:28 +0100

    refactor manage.py to work with core.pkgfile
    that simplifies code a bit since we work with the same datastructure.

--- pypiserver/manage.py
+++ pypiserver/manage.py
@@ -18,73 +18,81 @@ def is_stable_version(pversion):
 
 
 def filter_stable_releases(releases):
-    res = []
-    for pversion, version in releases:
-        if is_stable_version(pversion):
-            res.append((pversion, version))
-    return res
+    for pkg in releases:
+        if is_stable_version(pkg.parsed_version):
+            yield pkg
 
 
-def find_updates(pkgset, stable_only=True):
-    no_releases = set()
-
-    def write(s):
-        sys.stdout.write(s)
-        sys.stdout.flush()
-
-    pypi = Server("http://pypi.python.org/pypi/")
+def filter_latest_pkgs(pkgs):
     pkgname2latest = {}
 
-    for x in pkgset:
+    for x in pkgs:
         if x.pkgname not in pkgname2latest:
             pkgname2latest[x.pkgname] = x
         elif x.parsed_version > pkgname2latest[x.pkgname].parsed_version:
             pkgname2latest[x.pkgname] = x
 
-    need_update = []
+    return pkgname2latest.values()
 
-    sys.stdout.write("checking %s packages for newer version\n" % len(pkgname2latest),)
-    for count, (pkgname, file) in enumerate(pkgname2latest.items()):
+
+def build_releases(pkg, versions):
+    for x in versions:
+        parsed_version=core.parse_version(x)
+        if parsed_version > pkg.parsed_version:
+            yield core.pkgfile(version=x,
+                               parsed_version=parsed_version,
+                               pkgname=pkg.pkgname,
+                               replaces=pkg)
+
+
+def find_updates(pkgset, stable_only=True):
+    no_releases = set()
+    filter_releases = filter_stable_releases if stable_only else (lambda x: x)
+
+    def write(s):
+        sys.stdout.write(s)
+        sys.stdout.flush()
+
+    latest_pkgs = frozenset(filter_latest_pkgs(pkgset))
+
+    sys.stdout.write("checking %s packages for newer version\n" % len(latest_pkgs),)
+    need_update = set()
+    pypi = Server("http://pypi.python.org/pypi/")
+    for count, pkg in enumerate(latest_pkgs):
         if count % 40 == 0:
             write("\n")
 
-        releases = pypi.package_releases(pkgname)
-
-        releases = [(core.parse_version(x), x) for x in releases]
-        if stable_only:
-            releases = filter_stable_releases(releases)
-
-        status = "."
-        if releases:
-            m = max(releases)
-            if m[0] > file.parsed_version:
-                file.latest_version = m[1]
+        pypi_versions = pypi.package_releases(pkg.pkgname)
+        if pypi_versions:
+            releases = filter_releases(build_releases(pkg, pypi_versions))
+            status = "."
+            try:
+                need_update.add(max(releases, key=lambda x: x.parsed_version))
                 status = "u"
-                # print "%s needs update from %s to %s" % (pkgname, file.version, m[1])
-                need_update.append(file)
+            except ValueError:
+                pass
         else:
-            no_releases.add(pkgname)
             status = "e"
+            no_releases.add(pkg.pkgname)
 
         write(status)
 
     write("\n\n")
 
-    no_releases = list(no_releases)
     if no_releases:
-        no_releases.sort()
-        sys.stdout.write("no releases found on pypi for %s\n\n" % (", ".join(no_releases),))
+        sys.stdout.write("no releases found on pypi for %s\n\n" % (", ".join(sorted(no_releases)),))
+
     return need_update
 
 
 def update(pkgset, destdir=None, dry_run=False, stable_only=True):
     need_update = find_updates(pkgset, stable_only=stable_only)
-    for x in need_update:
-        sys.stdout.write("# update %s from %s to %s\n" % (x.pkgname, x.version, x.latest_version))
+    for pkg in sorted(need_update, key=lambda x: x.pkgname):
+        sys.stdout.write("# update %s from %s to %s\n" % (pkg.pkgname, pkg.replaces.version, pkg.version))
 
         cmd = ["pip", "-q", "install", "--no-deps", "-i", "http://pypi.python.org/simple",
-               "-d", destdir or os.path.dirname(x.fn),
-               "%s==%s" % (x.pkgname, x.latest_version)]
+               "-d", destdir or os.path.dirname(pkg.replaces.fn),
+               "%s==%s" % (pkg.pkgname, pkg.version)]
 
         sys.stdout.write("%s\n\n" % (" ".join(cmd),))
         if not dry_run:
