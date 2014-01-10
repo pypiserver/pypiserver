123b4937752f76b0e4ab2b291f6efc2b35377270
Ralf Schmitt <ralf@systemexit.de>
2014-01-10 01:30:10 +0100

    make pypiserver executable from wheel file
    i.e. python pypiserver-1.1.x-py2.py3-none-any.whl/pypiserver will now
    start pypiserver

--- pypiserver/__main__.py
+++ pypiserver/__main__.py
@@ -1,3 +1,6 @@
 if __name__ == "__main__":
+    if __package__ == "":  # running as python pypiserver-...whl/pypiserver?
+        import sys, os
+        sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
     from pypiserver import core
     core.main()
