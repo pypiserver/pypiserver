5c2ac82800d4017c6906691ecc8666860a013bb0
Christophe Taton <taton@wibidata.com>
2014-08-28 18:38:56 -0700

    Fix unauthenticated mode.
    When no htaccess files is specified, uploads always fail with a 403.

--- pypiserver/_app.py
+++ pypiserver/_app.py
@@ -27,7 +27,9 @@ config = configuration()
 
 
 def validate_user(username, password):
-    if config.htpasswdfile is not None:
+    if config.htpasswdfile is None:
+        return True
+    else:
         config.htpasswdfile.load_if_changed()
         return config.htpasswdfile.check_password(username, password)
 
