f8056565a837981d9a92160ea904df0739e641e1
Kostis Anagnostopoulos <ankostis@gmail.com>
2015-09-18 19:05:07 +0200

    Enable assert "dist-uploaded" in test_server.
    + Had not to use __future__.unicode_literals!
    + Refactor `test_server` using pytest fictures.

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,7 +1,5 @@
 #! /usr/bin/env py.test
 
-from __future__ import unicode_literals
-
 import contextlib
 import io
 import subprocess
@@ -10,11 +8,6 @@ import time
 
 from py import path  # @UnresolvedImport
 import pytest
-
-
-@pytest.fixture
-def packdir(tmpdir):
-    return tmpdir.mkdir("dists")
 
 
 @contextlib.contextmanager
@@ -31,27 +24,43 @@ def server(packdir):
             proc.kill()
 
 
-@pytest.mark.skipif(sys.version_info[:2] == (3, 2),
-                    reason="urllib3 fails on twine (see https://travis-ci.org/ankostis/pypiserver/builds/81044993)")
-def test_centodeps(packdir, monkeypatch):
-    from twine.commands import upload
+@pytest.fixture
+def packdir(tmpdir):
+    return tmpdir.mkdir("dists")
 
-    pypirc_config = {
+
+@pytest.fixture
+def pypirc():
+    return {
         "repository": "http://localhost:8080",
         "username": 'a',
         "password": 'a'
     }
 
+
+@pytest.fixture
+def uploader(pypirc, monkeypatch):
+    from twine.commands import upload
     monkeypatch.setattr(upload.utils, 'get_repository_from_config',
-                        lambda *x: pypirc_config)
+                        lambda *x: pypirc)
+
+    return upload
+
+
+@pytest.mark.skipif(sys.version_info[:2] == (3, 2),
+                    reason="urllib3 fails on twine (see https://travis-ci.org/ankostis/pypiserver/builds/81044993)")
+def test_centodeps(packdir, uploader):
+
     dist_path = path.local('tests/centodeps/wheelhouse/centodeps*.whl')
 
     with server(packdir) as srv:
         time.sleep(1)
-        upload.upload([str(dist_path)], repository='test',
-                      sign=None, identity=None,
-                      username='a', password='a',
-                      comment=None, sign_with=None,
-                      config_file=None, skip_existing=None)
+        uploader.upload([str(dist_path)], repository='test',
+                        sign=None, identity=None,
+                        username='a', password='a',
+                        comment=None, sign_with=None,
+                        config_file=None, skip_existing=None)
         time.sleep(1)
-    #assert list(packdir.visit('centodeps*.whl'))
+
+    assert list(packdir.visit('centodeps*.whl')), list(packdir.visit(
+                                                       'centodeps*.whl'))
