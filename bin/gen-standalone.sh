14c599773b6bbdc3b9619102e4e43973e99c6c51
Matthew Planchard <msplanchard@gmail.com>
2016-07-17 15:04:00 -0500

    * Attempt to update certs via travis

--- bin/gen-standalone.sh
+++ bin/gen-standalone.sh
@@ -15,13 +15,6 @@ python setup.py bdist_wheel
 wheel="./dist/pypiserver-*.whl"
 
 
-# Ensure we have up-to-date certificates
-if [ -e ./cacert.pem ]; then
-    rm -f ./cacert.pem
-fi
-
-wget http://curl.haxx.se/ca/cacert.pem
-
 ## Modify `wheel` archive with `__main__.py` at root,
 #   add dependencies, and
 #   prepend it with a python-flashbang + some header-comments >= 10-lines
@@ -30,9 +23,8 @@ wget http://curl.haxx.se/ca/cacert.pem
 unzip -jo $wheel pypiserver/__main__.py -d ./dist
 zip -d $wheel pypiserver/__main__.py
 zip -mj $wheel ./dist/__main__.py
-wget --ca-certificate ./cacert.pem \
-    https://pypi.python.org/packages/2.7/p/passlib/passlib-1.6.5-py2.py3-none-any.whl#md5=03de8f28697eaa67835758a60386c9fa \
-    -O ./dist/passlib-1.6.5-py2.py3-none-any.whl
+wget https://pypi.python.org/packages/2.7/p/passlib/passlib-1.6.5-py2.py3-none-any.whl#md5=03de8f28697eaa67835758a60386c9fa \
+        -O ./dist/passlib-1.6.5-py2.py3-none-any.whl
 zip -mj $wheel ./dist/passlib-*.whl
 gitversion=$(git describe --tags)
 cat  - $wheel > "$exec_zip" << EOF
