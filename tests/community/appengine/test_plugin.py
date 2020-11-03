486631893a38222936e42ff332aaca18e07a0bd0
Dmitrii <orlov@satelligence.com>
2020-11-03 22:26:33 +0100

    some fixes for black

--- tests/community/appengine/test_plugin.py
+++ tests/community/appengine/test_plugin.py
@@ -1,5 +1,5 @@
-'''Module responsible for testing the appengine plugin
-'''
+"""Module responsible for testing the appengine plugin
+"""
 
 # Standard library imports
 from unittest.mock import MagicMock, call
@@ -8,8 +8,10 @@ from unittest.mock import MagicMock, call
 import pytest
 
 # Local imports
-from pypiserver.community.appengine.plugin import (StoragePluginBuilder,
-                                                   SynchronizerPlugin)
+from pypiserver.community.appengine.plugin import (
+    StoragePluginBuilder,
+    SynchronizerPlugin,
+)
 from pypiserver.community.appengine.storage import BasicStorageClient
 
 
@@ -22,28 +24,37 @@ def storage_client():
     return MagicMock(spec=BasicStorageClient)
 
 
-def test_SynchronizerPlugin_sync_data_before_change_pulls_remote_files(storage_client: MagicMock):
+def test_SynchronizerPlugin_sync_data_before_change_pulls_remote_files(
+    storage_client: MagicMock,
+):
     plugin = _get_syncronizer_plugin(storage_client)
     plugin.sync_data_before_change()
     storage_client.pull_remote_files.assert_called_once_with()
 
 
-def test_SynchronizerPlugin_sync_data_before_change_stores_latest_local_files(storage_client: MagicMock):
+def test_SynchronizerPlugin_sync_data_before_change_stores_latest_local_files(
+    storage_client: MagicMock,
+):
     plugin = _get_syncronizer_plugin(storage_client)
     plugin.sync_data_before_change()
     storage_client.store_local_files_snapshot.assert_called_once_with()
 
 
-def test_SynchronizerPlugin_sync_data_after_change_gets_all_changes(storage_client: MagicMock):
+def test_SynchronizerPlugin_sync_data_after_change_gets_all_changes(
+    storage_client: MagicMock,
+):
     plugin = _get_syncronizer_plugin(storage_client)
     plugin.sync_data_after_change()
     storage_client.get_change_events.assert_called_once_with()
 
 
-def test_SynchronizerPlugin_sync_data_after_change_processes_all_changes(storage_client: MagicMock):
+def test_SynchronizerPlugin_sync_data_after_change_processes_all_changes(
+    storage_client: MagicMock,
+):
     expected_events = ['foo', 'bar']
     storage_client.get_change_events.return_value = expected_events
     plugin = _get_syncronizer_plugin(storage_client)
     plugin.sync_data_after_change()
     storage_client.upload_to_remote.assert_has_calls(
-        [call(event) for event in expected_events])
+        [call(event) for event in expected_events]
+    )
--- tests/community/appengine/test_storage.py
+++ tests/community/appengine/test_storage.py
@@ -1,5 +1,5 @@
-'''Module responsible for testing the appengine storage syncronization classes
-'''
+"""Module responsible for testing the appengine storage syncronization classes
+"""
 
 # Standard library imports
 from unittest.mock import MagicMock, call, patch
@@ -8,7 +8,35 @@ from unittest.mock import MagicMock, call, patch
 import pytest
 
 # Local imports
-from pypiserver.community.appengine.storage import BasicStorageClient, BaseFileStoreManager, ContentChangeTypes
+from pypiserver.community.appengine.storage import (
+    BaseFileStoreManager,
+    BasicStorageClient,
+    ContentChangeTypes,
+)
+
+REMOVAL_CHANGE_EVENT_PATCHER = patch(
+    'pypiserver.community.appengine.storage.RemovalChangeEvent'
+)
+ADDITION_CHANGE_EVENT_PATCHER = patch(
+    'pypiserver.community.appengine.storage.AdditionChangeEvent'
+)
+LAST_LOCAL_SNAPSHOT_PATCHER = patch.object(
+    BasicStorageClient, 'get_last_local_snapshot'
+)
+GET_LOCAL_CONTENTS_PATCHER = patch.object(
+    BasicStorageClient, 'get_local_contents'
+)
+
+
+def _stop_patchers(patchers=None):
+    if not patchers:
+        REMOVAL_CHANGE_EVENT_PATCHER.stop()
+        ADDITION_CHANGE_EVENT_PATCHER.stop()
+        LAST_LOCAL_SNAPSHOT_PATCHER.stop()
+        GET_LOCAL_CONTENTS_PATCHER.stop()
+    else:
+        for patcher in patchers:
+            patcher.stop()
 
 
 def _get_difference(*args, **kwargs):
@@ -24,43 +52,55 @@ def file_store_manager():
     return MagicMock(spec=BaseFileStoreManager)
 
 
-def test_BasicStorageClient_get_change_events_yields_all_removal_difference_events(file_store_manager):
+def test_BasicStorageClient_get_change_events_yields_all_removal_difference_events(
+    file_store_manager,
+):
     storage_client = _get_basic_storage_client(file_store_manager)
-    expected_snapshot = set(['a', 'b', 'c'])
-    expected_local_contents = set(['a', 'c'])
-    with patch('pypiserver.community.appengine.storage.RemovalChangeEvent') as mock_removal_change_event_class:
-        with patch('pypiserver.community.appengine.storage.AdditionChangeEvent') as mock_addition_change_event_class:
-            mock_removal_change_event_class.side_effect = _get_difference
-            mock_addition_change_event_class.side_effect = _get_difference
-            with patch.object(BasicStorageClient, 'get_last_local_snapshot') as mock_last_local_snapshot:
-                with patch.object(BasicStorageClient, 'get_local_contents') as mock_local_contents:
-                    expected_results = [mock_removal_change_event_class(difference={'b'}),
-                                        mock_addition_change_event_class(difference=set())]
-                    mock_last_local_snapshot.return_value = expected_snapshot
-                    mock_local_contents.return_value = expected_local_contents
-                    result = list(storage_client.get_change_events())
-                    assert expected_results == result
+    expected_snapshot = {'a', 'b', 'c'}
+    expected_local_contents = {'a', 'c'}
+    mock_removal_change_event_class = REMOVAL_CHANGE_EVENT_PATCHER.start()
+    mock_addition_change_event_class = ADDITION_CHANGE_EVENT_PATCHER.start()
+    mock_last_local_snapshot = LAST_LOCAL_SNAPSHOT_PATCHER.start()
+    mock_local_contents = GET_LOCAL_CONTENTS_PATCHER.start()
+    mock_removal_change_event_class.side_effect = _get_difference
+    mock_addition_change_event_class.side_effect = _get_difference
+    expected_results = [
+        mock_removal_change_event_class(difference={'b'}),
+        mock_addition_change_event_class(difference=set()),
+    ]
+    mock_last_local_snapshot.return_value = expected_snapshot
+    mock_local_contents.return_value = expected_local_contents
+    result = list(storage_client.get_change_events())
+    assert expected_results == result
+    _stop_patchers()
 
 
-def test_BasicStorageClient_get_change_events_yields_all_addition_difference_events(file_store_manager):
+def test_BasicStorageClient_get_change_events_yields_all_addition_difference_events(
+    file_store_manager,
+):
     storage_client = _get_basic_storage_client(file_store_manager)
-    expected_snapshot = set(['a', 'b', ])
-    expected_local_contents = set(['a', 'b', 'c'])
-    with patch('pypiserver.community.appengine.storage.RemovalChangeEvent') as mock_removal_change_event_class:
-        with patch('pypiserver.community.appengine.storage.AdditionChangeEvent') as mock_addition_change_event_class:
-            mock_removal_change_event_class.side_effect = _get_difference
-            mock_addition_change_event_class.side_effect = _get_difference
-            with patch.object(BasicStorageClient, 'get_last_local_snapshot') as mock_last_local_snapshot:
-                with patch.object(BasicStorageClient, 'get_local_contents') as mock_local_contents:
-                    expected_results = [mock_removal_change_event_class(difference=set()),
-                                        mock_addition_change_event_class(difference={'c'})]
-                    mock_last_local_snapshot.return_value = expected_snapshot
-                    mock_local_contents.return_value = expected_local_contents
-                    result = list(storage_client.get_change_events())
-                    assert expected_results == result
+    expected_snapshot = {'a', 'b'}
+    expected_local_contents = {'a', 'b', 'c'}
+    mock_removal_change_event_class = REMOVAL_CHANGE_EVENT_PATCHER.start()
+    mock_addition_change_event_class = ADDITION_CHANGE_EVENT_PATCHER.start()
+    mock_last_local_snapshot = LAST_LOCAL_SNAPSHOT_PATCHER.start()
+    mock_local_contents = GET_LOCAL_CONTENTS_PATCHER.start()
+    mock_removal_change_event_class.side_effect = _get_difference
+    mock_addition_change_event_class.side_effect = _get_difference
+    expected_results = [
+        mock_removal_change_event_class(difference=set()),
+        mock_addition_change_event_class(difference={'c'}),
+    ]
+    mock_last_local_snapshot.return_value = expected_snapshot
+    mock_local_contents.return_value = expected_local_contents
+    result = list(storage_client.get_change_events())
+    assert expected_results == result
+    _stop_patchers()
 
 
-def test_BasicStorageClient_upload_to_remote_calls_the_process_of_event(file_store_manager):
+def test_BasicStorageClient_upload_to_remote_calls_the_process_of_event(
+    file_store_manager,
+):
     storage_client = _get_basic_storage_client(file_store_manager)
     mock_event = MagicMock(spec=ContentChangeTypes)
     storage_client.upload_to_remote(mock_event)
