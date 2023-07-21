9e0419771760d5103b20861d7b0844b66507e93e
ghinks <ghinks@yahoo.com>
2023-07-21 08:46:04 -0400

    feat: markdown
    - update docker igore
    - alter unit test to look for version number after md changes

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -12,6 +12,6 @@ def readme():
 
 
 def test_READMEversion(readme):
-    m = re.compile(r"^\s*:Version:\s*(.+)\s*$", re.MULTILINE).search(readme)
+    m = re.compile(r"^\|\s*Version\s*\|\s*(\d+\.\d+\.\d+)\s*\|$", re.MULTILINE).search(readme)
     assert m, "Could not find version on README!"
     assert m.group(1) == my_ver, f"Updated version({m.group(1)}) on README!"
