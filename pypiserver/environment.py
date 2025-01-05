03b1c7e539daf1757438d4ddd08bf46a736c1d69
Dmitrii Orlov <dmtree.dev@yahoo.com>
2025-01-05 22:08:48 +0100

    fix: typing

--- pypiserver/environment.py
+++ pypiserver/environment.py
@@ -1,10 +1,11 @@
 import os
+from typing import Optional
 
 
 class Environment:
     """This class contains various environment configurations for Pypi-Server."""
 
-    PYPISERVER_BOTTLE_MEMFILE_MAX_OVERRIDE_BYTES: int | None = (
+    PYPISERVER_BOTTLE_MEMFILE_MAX_OVERRIDE_BYTES: Optional[int] = (
         int(override)
         if (
             override := os.getenv(
