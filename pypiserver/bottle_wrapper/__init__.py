853f24747371bc5e19732eb653499bbb804a3190
Dmitrii Orlov <dmtree.dev@yahoo.com>
2025-01-05 16:41:46 +0100

    chore(wip): add doctest to `env` overrides

--- pypiserver/bottle_wrapper/__init__.py
+++ pypiserver/bottle_wrapper/__init__.py
@@ -3,8 +3,8 @@ This `__init__.py` allows to wrap and patch the default `bottle.py` implementati
 """
 
 from pypiserver.bottle_wrapper.bottle import *
-from pypiserver.environment import Environment
+from pypiserver.environment import ENVIRONMENT
 
 BaseRequest.MEMFILE_MAX = (
-    Environment.PYPISERVER_BOTTLE_MEMFILE_MAX_OVERRIDE_BYTES
+    ENVIRONMENT.PYPISERVER_BOTTLE_MEMFILE_MAX_OVERRIDE_BYTES
 )
--- pypiserver/environment.py
+++ pypiserver/environment.py
@@ -2,12 +2,33 @@ import os
 from pypiserver.bottle_wrapper.bottle import BaseRequest
 
 
-class Environment:
+class __Environment__:
     """This class contains various environment configurations for Pypi-Server."""
 
-    PYPISERVER_BOTTLE_MEMFILE_MAX_OVERRIDE_BYTES: int = int(
-        os.getenv(
-            "PYPISERVER_BOTTLE_MEMFILE_MAX_OVERRIDE_BYTES",
-            BaseRequest.MEMFILE_MAX,
+    @property
+    def PYPISERVER_BOTTLE_MEMFILE_MAX_OVERRIDE_BYTES(self) -> int:
+        """A way to override the `MEMFILE_MAX` value set in `bottle`.
+
+        Examples:
+            - `PYPISERVER_BOTTLE_MEMFILE_MAX_OVERRIDE_BYTES`
+
+            -- Default Value is 100 KB
+            >>> environment = __Environment__()
+            >>> environment.PYPISERVER_BOTTLE_MEMFILE_MAX_OVERRIDE_BYTES
+            102400
+
+            -- Can be patched via environment variables
+            >>> new_value_100_mb = (2 ** 20) * 100
+            >>> os.environ["PYPISERVER_BOTTLE_MEMFILE_MAX_OVERRIDE_BYTES"] = str(new_value_100_mb)
+            >>> environment.PYPISERVER_BOTTLE_MEMFILE_MAX_OVERRIDE_BYTES
+            104857600
+        """
+        return int(
+            os.getenv(
+                "PYPISERVER_BOTTLE_MEMFILE_MAX_OVERRIDE_BYTES",
+                BaseRequest.MEMFILE_MAX,
+            )
         )
-    )
+
+
+ENVIRONMENT = __Environment__()
