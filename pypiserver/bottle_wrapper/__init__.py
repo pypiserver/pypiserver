be56c8f7a8c798f7bbf4779d53a66da91461152c
Dmitrii Orlov <dmtree.dev@yahoo.com>
2025-07-18 18:03:14 +0200

    chore: remove the override flag

--- pypiserver/bottle_wrapper/__init__.py
+++ pypiserver/bottle_wrapper/__init__.py
@@ -3,10 +3,3 @@ This `__init__.py` allows to wrap and patch the default `bottle.py` implementati
 """
 
 from pypiserver.bottle_wrapper.bottle import *
-from pypiserver.environment import Environment
-
-BaseRequest.MEMFILE_MAX = (
-    override
-    if (override := Environment.PYPISERVER_BOTTLE_MEMFILE_MAX_OVERRIDE_BYTES)
-    else BaseRequest.MEMFILE_MAX
-)
